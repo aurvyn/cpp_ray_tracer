dcda1b84842c202fcd6b93c139c8bd81d1d1a88d
petersa2 <petersa2@rose-hulman.edu>
2025-02-03 14:24:18 -0500

    Add effect ideas

--- src/Effect.h
+++ src/Effect.h
@@ -4,6 +4,25 @@
 #include <list>
 #include "HSV.h"
 
+/*
+Effect ideas
+High pass / low pass filters
+Bloom
+Toon shading
+Thick outlines via edge detection (requires depth / normal map)
+Photoshop effects
+    Overlay
+    Add
+    Subtract
+    Multiply
+
+Vignette
+Dithering
+Depth of field
+    
+
+*/
+
 class Effect
 {
     public:
