53212e910d4e170db7a5f11f62323be0b2afa547
Aurvyn <admin@dysrakescence.eu.org>
2025-02-12 14:44:59 -0500

    Refactor: Split `dilate` into seperate methods

--- src/MotionBuffer.h
+++ src/MotionBuffer.h
@@ -22,52 +22,8 @@ public:
     MotionBuffer *dilate(int samples = 32, int chunkSize = 16) {
         int width = this->getWidth();
         int height = this->getHeight();
-        Buffer<Vector2> neighborBuffer(width/chunkSize + 1, height/chunkSize + 1);
+        Buffer<Vector2> neighborHood = getNeighborhood(chunkSize);
         MotionBuffer finalBuffer(width, height);
-        for (int i = 0; i < neighborBuffer.getWidth(); ++i) {
-            for (int j = 0; j < neighborBuffer.getHeight(); ++j) {
-                int offset_x = i*chunkSize;
-                int offset_y = j*chunkSize;
-                int chunkWidth = std::min(chunkSize, width - offset_x);
-                int chunkHeight = std::min(chunkSize, height - offset_y);
-                float highestMagnitude = 0.0f;
-                Vector2 guidingVelocity = this->at(offset_x, offset_y);
-                for (int m = 0; m < chunkWidth; ++m) {
-                    for (int n = 0; n < chunkHeight; ++n) {
-                        Vector2 motion = this->at(offset_x+m, offset_y+n);
-                        float magnitude = motion.length();
-                        if (magnitude > highestMagnitude) {
-                            highestMagnitude = magnitude;
-                            guidingVelocity = motion;
-                        }
-                    }
-                }
-                neighborBuffer.at(i, j) = guidingVelocity;
-            }
-        }
-        Buffer<Vector2> neighborHood(neighborBuffer.getWidth(), neighborBuffer.getHeight());
-        for (int i = 0; i < neighborBuffer.getWidth(); ++i) {
-            for (int j = 0; j < neighborBuffer.getHeight(); ++j) {
-                Vector2 guidingVelocity = neighborBuffer.at(i, j);
-                Vector2 neighborVelocity = Vector2(0.0f);
-                float highestMagnitude = 0.0f;
-                for (int k = -1; k <= 1; ++k) {
-                    for (int l = -1; l <= 1; ++l) {
-                        int sx = i + k;
-                        int sy = j + l;
-                        if (sx >= 0 && sx < neighborBuffer.getWidth()
-                         && sy >= 0 && sy < neighborBuffer.getHeight()) {
-                            float magnitude = neighborBuffer.at(sx, sy).length();
-                            if (magnitude > highestMagnitude) {
-                                highestMagnitude = magnitude;
-                                neighborVelocity = neighborBuffer.at(sx, sy);
-                            }
-                        }
-                    }
-                }
-                neighborHood.at(i, j) = neighborVelocity;
-            }
-        }
         for (int i = 0; i < neighborHood.getWidth(); ++i) {
             for (int j = 0; j < neighborHood.getHeight(); ++j) {
                 int offset_x = i*chunkSize;
@@ -93,5 +49,62 @@ public:
         }
         *this = finalBuffer;
         return this;
+    }
+
+private:
+    Buffer<Vector2> getNeighborBuffer(int chunkSize) {
+        int width = this->getWidth();
+        int height = this->getHeight();
+        Buffer<Vector2> neighborBuffer(width/chunkSize + 1, height/chunkSize + 1);
+        for (int i = 0; i < neighborBuffer.getWidth(); ++i) {
+            for (int j = 0; j < neighborBuffer.getHeight(); ++j) {
+                int offset_x = i*chunkSize;
+                int offset_y = j*chunkSize;
+                int chunkWidth = std::min(chunkSize, width - offset_x);
+                int chunkHeight = std::min(chunkSize, height - offset_y);
+                float highestMagnitude = 0.0f;
+                Vector2 guidingVelocity = this->at(offset_x, offset_y);
+                for (int m = 0; m < chunkWidth; ++m) {
+                    for (int n = 0; n < chunkHeight; ++n) {
+                        Vector2 motion = this->at(offset_x+m, offset_y+n);
+                        float magnitude = motion.length();
+                        if (magnitude > highestMagnitude) {
+                            highestMagnitude = magnitude;
+                            guidingVelocity = motion;
+                        }
+                    }
+                }
+                neighborBuffer.at(i, j) = guidingVelocity;
+            }
+        }
+        return neighborBuffer;
+    }
+
+    Buffer<Vector2> getNeighborhood(int chunkSize) {
+        Buffer<Vector2> neighborBuffer = getNeighborBuffer(chunkSize);
+        Buffer<Vector2> neighborHood(neighborBuffer.getWidth(), neighborBuffer.getHeight());
+        for (int i = 0; i < neighborBuffer.getWidth(); ++i) {
+            for (int j = 0; j < neighborBuffer.getHeight(); ++j) {
+                Vector2 guidingVelocity = neighborBuffer.at(i, j);
+                Vector2 neighborVelocity = Vector2(0.0f);
+                float highestMagnitude = 0.0f;
+                for (int k = -1; k <= 1; ++k) {
+                    for (int l = -1; l <= 1; ++l) {
+                        int sx = i + k;
+                        int sy = j + l;
+                        if (sx >= 0 && sx < neighborBuffer.getWidth()
+                         && sy >= 0 && sy < neighborBuffer.getHeight()) {
+                            float magnitude = neighborBuffer.at(sx, sy).length();
+                            if (magnitude > highestMagnitude) {
+                                highestMagnitude = magnitude;
+                                neighborVelocity = neighborBuffer.at(sx, sy);
+                            }
+                        }
+                    }
+                }
+                neighborHood.at(i, j) = neighborVelocity;
+            }
+        }
+        return neighborHood;
     }
 };
